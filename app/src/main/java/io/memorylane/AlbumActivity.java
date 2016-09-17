e856c2be8659757081b848ad6f34ec43a27bfa97
Jacek Bylina <jacek.bylina@gmail.com>
2016-09-17 06:20:09 +0200

    Database and adding albums feature

--- app/src/main/java/io/memorylane/AlbumActivity.java
+++ app/src/main/java/io/memorylane/AlbumActivity.java
@@ -1,12 +1,17 @@
 package io.memorylane;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -18,20 +23,30 @@ import android.view.View;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.memorylane.model.Album;
 import io.memorylane.model.AlbumModel;
+import io.memorylane.model.Asset;
 import io.memorylane.view.AlbumAdapter;
 import io.memorylane.view.HideScrollListener;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 /**
  * Created by abertschi on 17/09/16.
  */
 public class AlbumActivity extends AppCompatActivity {
 
+    public static final int PERMISSIONS_REQUEST_TO_READ_CONTACTS = 100;
+
     private RecyclerView mRecycleView;
     private AlbumAdapter mAlbumAdapter;
     private Toolbar mToolbar;
     private AppBarLayout mAppBarLayout;
     private FloatingActionButton mAddAlbumButton;
+    private Realm mRealm;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,7 +62,11 @@ public class AlbumActivity extends AppCompatActivity {
                         .input("Album Name", "", new MaterialDialog.InputCallback() {
                             @Override
                             public void onInput(MaterialDialog dialog, CharSequence input) {
-                                // Do something
+                                mRealm.beginTransaction();
+                                Album album = mRealm.createObject(Album.class);
+                                album.setName(input.toString());
+                                mRealm.commitTransaction();
+                                mAlbumAdapter.notifyDataSetChanged();
                             }
                         })
                         .build();
@@ -65,8 +84,29 @@ public class AlbumActivity extends AppCompatActivity {
             }
         });
 
+        initDatabase(this);
         initRecycleView();
         initToolbar();
+        initPermissions();
+    }
+
+    private void initDatabase(Context context){
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder(context).build();
+        Realm.setDefaultConfiguration(realmConfig);
+        mRealm = Realm.getDefaultInstance();
+    }
+
+    private void initPermissions() {
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // TODO: add explanation
+            }
+
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_TO_READ_CONTACTS);
+        } else {
+            getImageAssets();
+        }
     }
 
     private void initToolbar() {
@@ -79,8 +119,7 @@ public class AlbumActivity extends AppCompatActivity {
     private void initRecycleView() {
         mRecycleView = (RecyclerView) findViewById(R.id.album_recycler_view);
         mRecycleView.setLayoutManager(new LinearLayoutManager(this));
-        AlbumModel model = AlbumModel.createData();
-        mAlbumAdapter = new AlbumAdapter(model, this);
+        mAlbumAdapter = new AlbumAdapter(new AlbumModel(), this);
         mRecycleView.setAdapter(mAlbumAdapter);
         mRecycleView.addOnScrollListener(new HideScrollListener() {
             @Override
@@ -96,5 +135,49 @@ public class AlbumActivity extends AppCompatActivity {
                 mAddAlbumButton.animate().translationY(0);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_TO_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getImageAssets();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private List<Asset> getImageAssets () {
+        return getImageAssets(this);
+    }
+
+    private List<Asset> getImageAssets(Activity activity) {
+        ArrayList<Asset> assets = new ArrayList<>();
+
+        try(Cursor cursor = activity.getContentResolver().query( MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new  String[]{MediaStore.MediaColumns.DATA},
+                null,
+                null,
+                null)){
+            if (cursor != null) {
+
+                int data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+                while (cursor.moveToNext()) {
+                    assets.add(new Asset(cursor.getString(data)));
+                }
+            }
+        }
+        return assets;
     }
 }
--- app/src/main/java/io/memorylane/MainActivity.java
+++ app/src/main/java/io/memorylane/MainActivity.java
@@ -19,11 +19,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import io.memorylane.model.Asset;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final int PERMISSIONS_REQUEST_TO_READ_CONTACTS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,68 +40,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-
-            }
-
-            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_TO_READ_CONTACTS);
-        }
-
-        getImageAssets();
     }
 
-    private List<File> getImageAssets () {
-        return getAllShownImagesPath(this);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_TO_READ_CONTACTS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getImageAssets();
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
-
-
-    private List<File> getAllShownImagesPath(Activity activity) {
-        ArrayList<File> images = new ArrayList<>();
-
-        try(Cursor cursor = activity.getContentResolver().query( MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                new  String[]{MediaStore.MediaColumns.DATA},
-                null,       // Return all rows
-                null,
-                null)){
-            if (cursor != null) {
-
-                int data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-                while (cursor.moveToNext()) {
-
-                    File file = new File(cursor.getString(data));
-
-
-                    images.add(file);
-                }
-            }
-        }
-
-        return images;
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
