31320cc152a32600a1e30f7c8fefd6a33c78beb6
Jacek Bylina <jacek.bylina@gmail.com>
2016-09-17 04:11:11 +0200

    Get paths to files

--- app/src/main/java/io/memorylane/MainActivity.java
+++ app/src/main/java/io/memorylane/MainActivity.java
@@ -1,6 +1,11 @@
 package io.memorylane;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +14,16 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final int PERMISSIONS_REQUEST_TO_READ_CONTACTS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +40,69 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            }
+
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_TO_READ_CONTACTS);
+        }
+
+        getImageAssets();
     }
 
+    private List<File> getImageAssets () {
+        return getAllShownImagesPath(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_TO_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getImageAssets();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
+    private List<File> getAllShownImagesPath(Activity activity) {
+        ArrayList<File> images = new ArrayList<>();
+
+        try(Cursor cursor = activity.getContentResolver().query( MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new  String[]{MediaStore.MediaColumns.DATA},
+                null,       // Return all rows
+                null,
+                null)){
+            if (cursor != null) {
+
+                int data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+                while (cursor.moveToNext()) {
+
+                    File file = new File(cursor.getString(data));
+
+
+                    images.add(file);
+                }
+            }
+        }
+
+        return images;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
