a5db016be138b92af9c7079f0f9eb47d3e8f783d
Andrin <andrin.bertschi@gmail.com>
2016-09-17 05:17:02 +0200

    add dialg to add album, general improvements

--- app/src/main/java/io/memorylane/AlbumActivity.java
+++ app/src/main/java/io/memorylane/AlbumActivity.java
@@ -1,13 +1,24 @@
 package io.memorylane;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.KeyEvent;
+import android.view.View;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import io.memorylane.model.AlbumModel;
 import io.memorylane.view.AlbumAdapter;
+import io.memorylane.view.HideScrollListener;
 
 /**
  * Created by abertschi on 17/09/16.
@@ -16,20 +27,72 @@ public class AlbumActivity extends AppCompatActivity {
 
     private RecyclerView mRecycleView;
     private AlbumAdapter mAlbumAdapter;
+    private Toolbar mToolbar;
+    private AppBarLayout mAppBarLayout;
+    private FloatingActionButton mAddAlbumButton;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.album_activity);
+        mAddAlbumButton = (FloatingActionButton) findViewById(R.id.fab);
+        mAddAlbumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MaterialDialog dialog = new MaterialDialog.Builder(AlbumActivity.this)
+                        .title("Create new album")
+                        .inputType(InputType.TYPE_CLASS_TEXT)
+                        .input("Album Name", "", new MaterialDialog.InputCallback() {
+                            @Override
+                            public void onInput(MaterialDialog dialog, CharSequence input) {
+                                // Do something
+                            }
+                        })
+                        .build();
+                dialog.show();
 
-        initAlbums();
+                dialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+                    @Override
+                    public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+                        dialog.dismiss();
+//                        Snackbar.make(mRecycleView, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                                .setAction("Action", null).show();
+                        return false;
+                    }
+                });
+            }
+        });
+
+        initRecycleView();
+        initToolbar();
     }
 
-    private void initAlbums() {
+    private void initToolbar() {
+        mAppBarLayout = (AppBarLayout) findViewById(R.id.appbarLayout);
+        mToolbar = (Toolbar) findViewById(R.id.memorylane_toolbar);
+        setSupportActionBar(mToolbar);
+        setTitle(getString(R.string.app_name));
+    }
+
+    private void initRecycleView() {
         mRecycleView = (RecyclerView) findViewById(R.id.album_recycler_view);
         mRecycleView.setLayoutManager(new LinearLayoutManager(this));
         AlbumModel model = AlbumModel.createData();
         mAlbumAdapter = new AlbumAdapter(model);
         mRecycleView.setAdapter(mAlbumAdapter);
+        mRecycleView.addOnScrollListener(new HideScrollListener() {
+            @Override
+            public void onHide() {
+                mAppBarLayout.animate().translationY((float) (-mAppBarLayout.getHeight() * 1.5));
+                mAddAlbumButton.animate().translationY(mAddAlbumButton.getHeight() * 2);
+
+            }
+
+            @Override
+            public void onShow() {
+                mAppBarLayout.animate().translationY(0);
+                mAddAlbumButton.animate().translationY(0);
+            }
+        });
     }
 }
---
+++ app/src/main/java/io/memorylane/view/PictureInPictureView.java
@@ -0,0 +1,7 @@
+package io.memorylane.view;
+
+/**
+ * Created by abertschi on 17/09/16.
+ */
+public class PictureInPictureView {
+}
